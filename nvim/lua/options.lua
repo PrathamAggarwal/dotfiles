3eaff25c1060989d57e84be6b78a5112f5c293a9
PrathamAggarwal <51155223+PrathamAggarwal@users.noreply.github.com>
2026-01-08 22:24:30 +0530

    Add files via upload

---
+++ nvim/lua/options.lua
@@ -0,0 +1,8 @@
+require "nvchad.options"
+
+-- add yours here!
+vim.wo.relativenumber = true
+vim.opt.clipboard = ""
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,65 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+
+
+    -- Lualine
+    {
+    "nvim-lualine/lualine.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "seoul256",
+        },
+      })
+    end,
+  },
+
+  -- Markdown Preview
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+
+  -- Nvim Surround
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+
+}
